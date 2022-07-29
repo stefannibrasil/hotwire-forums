0f7e065b8b1e19a59691e9612c799d1796a9e605
Stefanni Brasil <stefannibrasil@gmail.com>
2022-07-29 16:25:01 -0700

    Realtime posts creation from a discussion page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :discussion, counter_cache: true, touch: true
   belongs_to :user, default: -> { Current.user }
+
   has_rich_text :body
 
   validates :body, presence: true
+
+  after_create_commit -> {
+    broadcast_append_to discussion,
+    partial: "discussions/posts/post",
+    locals: { post: self }
+  }
 end
