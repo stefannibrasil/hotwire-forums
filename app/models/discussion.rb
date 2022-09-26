e5ea420b0c738a6b54baad5a69aa6a6316d0a01d
Stefanni Brasil <stefannibrasil@gmail.com>
2022-09-25 17:27:30 -0700

    Update discussion model to toggle subscription status

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -5,6 +5,16 @@ class Discussion < ApplicationRecord
   delegate :name, prefix: :category, to: :category, allow_nil: true
 
   has_many :posts, dependent: :destroy
+  has_many :users, through: :posts
+  has_many :discussion_subscriptions, dependent: :destroy
+  has_many :optin_subscribers,
+    -> { where(discussion_subscriptions: { subscription_type: :optin }) },
+    through: :discussion_subscriptions,
+    source: :user
+  has_many :optout_subscribers,
+    -> { where(discussion_subscriptions: { subscription_type: :optout }) },
+    through: :discussion_subscriptions,
+    source: :user
 
   validates :name, presence: true
 
@@ -18,5 +28,24 @@ class Discussion < ApplicationRecord
 
   def to_param
     "#{id}-#{name.downcase.to_s[0...100]}".parameterize
+  end
+
+  def subscribed_users
+    (users + optin_subscribers).uniq - optout_subscribers
+  end
+
+  def subscription_for(user)
+    return nil if user.nil?
+    discussion_subscriptions.find_by(user_id: user.id)
+  end
+
+  def toggle_subscription(user)
+    if subscription = subscription_for(user)
+      subscription.toggle!
+    elsif posts.where(user_id: user.id).any?
+      discussion_subscriptions.create(user: user, subscription_type: "optout")
+    else
+      discussion_subscriptions.create(user: user, subscription_type: "optin")
+    end
   end
 end
